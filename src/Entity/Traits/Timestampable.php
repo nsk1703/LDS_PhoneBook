9949003d3346cd08064580e5eadb864319104c82
nsk1703 <kevinsamuelndoum@gmail.com>
2021-02-18 08:49:12 +0100

    Adding of updatedAt field and making migration

--- src/Entity/Traits/Timestampable.php
+++ src/Entity/Traits/Timestampable.php
@@ -39,6 +39,11 @@ Trait Timestampable
      */
     private $createdAt;
 
+    /**
+     * @ORM\Column(type="datetime", options={"default": "CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
+
 
     public function getId(): ?int
     {
@@ -117,8 +122,21 @@ Trait Timestampable
         return $this;
     }
 
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
     /**
      * @ORM\PrePersist
+     * @ORM\PreUpdate
      */
     public function createTimestamps()
     {
@@ -126,6 +144,7 @@ Trait Timestampable
         {
             $this->setCreatedAt(new \DateTimeImmutable());
         }
+        $this->setUpdatedAt(new \DateTimeImmutable());
 
     }
 }
