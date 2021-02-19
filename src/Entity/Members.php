390666c05c640b385632dd3a176a42abe9ee5397
nsk1703 <kevinsamuelndoum@gmail.com>
2021-02-19 19:43:14 +0100

    Installation and configuration of vichUploader

--- src/Entity/Members.php
+++ src/Entity/Members.php
@@ -5,11 +5,13 @@ namespace App\Entity;
 use App\Entity\Traits\Timestampable;
 use App\Repository\MembersRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass=MembersRepository::class)
  * @ORM\Table(name="members")
  * @ORM\HasLifecycleCallbacks
+ * @Vich\Uploadable
  */
 class Members
 {
