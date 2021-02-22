60b878dd33c8310c6c7f2f44da094290fed712fb
nsk1703 <kevinsamuelndoum@gmail.com>
2021-02-22 13:47:17 +0100

    Adding of attributes imageName in form and modiy of link layout

--- src/Form/MembersType.php
+++ src/Form/MembersType.php
@@ -6,12 +6,20 @@ use App\Entity\Members;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class MembersType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'image (JPG or PNG file)',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => false,
+//                'imagine_pattern' => 'squared_thumbnail_small',
+            ])
             ->add('first_name')
             ->add('last_name')
             ->add('phone_number')
